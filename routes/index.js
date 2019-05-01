1f980f5b7be4f8edfbf2b2c72ad9e88680336fd3
Emi Oda <emoda@ucsd.edu>
2019-05-01 00:47:55 -0700

    .

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 // load JSON fake "database" file into the data variable
-var data = require('../data.json');
+//var data = require('../data.json');
 
 exports.home = function(req, res){
   res.render('home');
